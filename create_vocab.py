c2a31f0490c61fc9f8e907e220f8a1f7d5ff16b8
Quoc Duong Nguyen <quocduong.nguyen01@estudiant.upf.edu>
2023-07-01 22:27:09 +0200

    fix: adapt mapping creation for added difficulty token

--- create_vocab.py
+++ create_vocab.py
@@ -8,7 +8,7 @@ def get_unique_strings(lists_of_strings):
         return []
 
     unique_strings = set()
-    for sub_list in lists_of_strings:
+    for _, sub_list in lists_of_strings:
         for string in sub_list:
             if string is not None:
                 unique_strings.add(string)
@@ -22,10 +22,12 @@ def write_to_file(strings, filename):
 
 
 def create_mappings(output_folder, tokens_list, unique_tokens):
-    for i, tokens in enumerate(tokens_list):
-        with open(os.path.join(output_folder, 'file' + str(i).zfill(3)) + '.txt', 'w') as file:
-            for token in tokens:
+    for i, obj in enumerate(tqdm(tokens_list, 'Create mapping files')):
+        path, tokens = obj
+        with open(os.path.join(output_folder, os.path.splitext(os.path.basename(path))[0] + '.txt'), 'w') as f:
+            f.write(str(tokens[0]) + ' ')
+            for i in range(1, len(tokens)):
                 try:
-                    file.write(str(unique_tokens.index(token)) + ' ')
+                    f.write(str(unique_tokens.index(tokens[i])) + ' ')
                 except ValueError as v:
                     continue
